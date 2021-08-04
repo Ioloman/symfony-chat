cc0cf5be5134d6620aff89c2758e016de64e6130
Kirill <kirilldd2@gmail.com>
2021-08-04 13:41:01 +0700

    added user model, user fixtures and basic authentication;

---
+++ src/Factory/UserFactory.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Zenstruck\Foundry\RepositoryProxy;
+use Zenstruck\Foundry\ModelFactory;
+use Zenstruck\Foundry\Proxy;
+
+/**
+ * @method static User|Proxy createOne(array $attributes = [])
+ * @method static User[]|Proxy[] createMany(int $number, $attributes = [])
+ * @method static User|Proxy find($criteria)
+ * @method static User|Proxy findOrCreate(array $attributes)
+ * @method static User|Proxy first(string $sortedField = 'id')
+ * @method static User|Proxy last(string $sortedField = 'id')
+ * @method static User|Proxy random(array $attributes = [])
+ * @method static User|Proxy randomOrCreate(array $attributes = [])
+ * @method static User[]|Proxy[] all()
+ * @method static User[]|Proxy[] findBy(array $attributes)
+ * @method static User[]|Proxy[] randomSet(int $number, array $attributes = [])
+ * @method static User[]|Proxy[] randomRange(int $min, int $max, array $attributes = [])
+ * @method static UserRepository|RepositoryProxy repository()
+ * @method User|Proxy create($attributes = [])
+ */
+final class UserFactory extends ModelFactory
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+    }
+
+    public function adminUsers(): self
+    {
+        return $this->addState([
+            'roles' => ["ROLE_ADMIN"],
+            'email' => 'admin@sibers.com'
+        ]);
+    }
+
+    protected function getDefaults(): array
+    {
+        return [
+            'email' => self::faker()->email(),
+            'firstName' => self::faker()->firstName(),
+            'lastName' => self::faker()->lastName(),
+            'password' => '1234',
+            'profilePic' => 'http://lorempixel.com/640/640/animals/',
+        ];
+    }
+
+    protected function initialize(): self
+    {
+        // see https://github.com/zenstruck/foundry#initialization
+        return $this
+            // ->afterInstantiate(function(User $user) {})
+        ;
+    }
+
+    protected static function getClass(): string
+    {
+        return User::class;
+    }
+}
