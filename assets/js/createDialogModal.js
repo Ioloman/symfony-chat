3a4c47c0e64b2b9ee80c35560c9da820d8167018
Kirill <kirilldd2@gmail.com>
2021-08-04 18:18:41 +0700

    added dialog creation via ajax request;

--- assets/js/createDialogModal.js
+++ assets/js/createDialogModal.js
@@ -4,8 +4,28 @@ $(function () {
     $('#createChatroomButton').click(function (e) {
         e.preventDefault();
         const $form = $(this).parent().parent();
-        if (isNaN(Number.parseInt($form.find('#selectUser')))) {
+        if (isNaN(Number.parseInt($form.find('#selectUser').val()))) {
             $form.addClass('was-validated');
+        } else {
+            $form.removeClass('was-validated');
+
+            $.ajax(
+                {
+                    url: $(this).data('controller-url'),
+                    method: 'POST',
+                    data: $form.serialize()
+                }
+            ).done(function (data) {
+                if (data['status'] === 'success') {
+                    window.location.href = data['url'];
+                } else {
+                    alert(data['message']);
+                }
+            }).fail(function () {
+                alert('Internal Error!');
+            })
+            ;
+            (new bootstrap.Modal(document.getElementById('newDialogModal'))).hide();
         }
     });
 });
--- src/Controller/RoomController.php
+++ src/Controller/RoomController.php
@@ -2,18 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\Chatroom;
 use App\Repository\ChatroomRepository;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class RoomController extends AbstractController
 {
-    public function index(): Response
+    public function index(Chatroom $chatroom): Response
     {
         return $this->render('room/index.html.twig', [
-
+            'chatroom' => $chatroom
         ]);
     }
 
@@ -23,5 +27,33 @@ class RoomController extends AbstractController
             'chatrooms' => $chatroomRepository->findAll(),
             'users' => $userRepository->findAll(),
         ]);
+    }
+
+    public function create(
+        Request $request,
+        UserRepository $userRepository,
+        EntityManagerInterface $entityManager,
+        UrlGeneratorInterface $urlGenerator
+    ): Response {
+        $user = $userRepository->find($request->request->get('email'));
+        if ($user) {
+            $room = new Chatroom();
+            $name = $request->request->get('name');
+            $name = trim($name) === "" ? null : $name;
+            $room->setName($name);
+            $room->addUser($user);
+            $room->addUser($this->getUser());
+
+            $entityManager->persist($room);
+            $entityManager->flush();
+
+            return $this->json([
+                'status' => 'success',
+                'message' => 'Chatroom was successfully created!',
+                'url' => $urlGenerator->generate('roomView', ['id' => $room->getId()])
+            ]);
+        } else {
+            return $this->json(['status' => 'failure', 'message' => "Internal Error! Chatroom wasn't created"]);
+        }
     }
 }
