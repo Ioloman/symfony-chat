a85b88574293a368b84db7926ad80dc22146c229
Kirill <kirilldd2@gmail.com>
2021-08-10 17:01:15 +0700

    added profile form and endpoint;

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserProfileType;
+use Gedmo\Sluggable\Util\Urlizer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -27,5 +32,42 @@ class SecurityController extends AbstractController
     public function logout()
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    public function profile(Request $request): Response
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+        $form = $this->createForm(UserProfileType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            /** @var UploadedFile $uploadedFile */
+            $uploadedFile = $form['profilePicName']->getData();
+
+            if ($uploadedFile) {
+
+                $destination = $this->getParameter('kernel.project_dir').'/public/uploads/user_pic';
+                $originalFilename = pathinfo($uploadedFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $newFilename = Urlizer::urlize($originalFilename).'-'.uniqid().'.'.$uploadedFile->guessExtension();
+
+                $uploadedFile->move($destination, $newFilename);
+
+                $user->setProfilePic($newFilename);
+            }
+            $formUser = $form->getData();
+            $user->setFirstName($formUser->getFirstName());
+            $user->setLastName($formUser->getLastName());
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+        }
+
+        return $this->render('security/profile.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
